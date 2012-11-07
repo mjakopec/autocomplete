8e9aec790233cd99d4b7903e574b179dda568d11
unknown <faveo@.(none)>
2012-11-07 12:12:46 +0100

    initi

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.2.8'
+
+gem 'jquery-rails'
+gem 'rails3-jquery-autocomplete', :path => '../'
+gem 'sqlite3-ruby', :require => 'sqlite3'
+gem 'mysql2'
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Integration::Application.routes.draw do
+
+  
+  root :to => 'autocomplete#new'
+  match 'autocomplete/autocomplete_brand_name' => 'autocomplete#autocomplete_brand_name'
+
+  resources :products
+  end
